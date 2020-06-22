d29ac8f905c2ecf10daa6c8075a828aec7837dcc
SaundaleB1995 <62260690+SaundaleB1995@users.noreply.github.com>
2020-06-22 21:03:05 +0530

    update endpoint in db host name

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': 'myDatabase',
         'USER': 'Bhushan',
         'PASSWORD': 'Bhushan123',
-        'HOST': 'terraform-20200621182509799400000001.cdhdu8rbe3nd.ap-south-1.rds.amazonaws.com',
+        'HOST': 'terraform-20200622152506966500000001.cdhdu8rbe3nd.ap-south-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
